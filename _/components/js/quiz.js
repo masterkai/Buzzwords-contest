64c8c8ed3a21f487d99c514f32610f46523c5527
masterkai <kai0919@hotmail.com>
2017-05-16 11:07:29 +0800

    Build Question & Answer

--- _/components/js/quiz.js
+++ _/components/js/quiz.js
@@ -2,8 +2,10 @@ var quizData = 'https://api.myjson.com/bins/v3b3d';
 //https://api.myjson.com/bins/v3b3d
 
 var output = document.getElementById('output');
+var bAnswer = document.getElementsByClassName('btnAns');
 var myObj='',
-    page=0;
+    page=0,
+    correctAnswer = 0;
 loadQuestion();
 
 function loadQuestion() {
@@ -12,16 +14,32 @@ function loadQuestion() {
     a.onreadystatechange = function () {
         if(a.readyState==4){
             myObj = JSON.parse(a.responseText);
-            page =1;
-            buildQuiz(1);
+            buildQuiz(0);
         }
     }
     a.send();
 }
 
-function buildQuiz(pg) {
-    for (var i in myObj){
-        console.log(myObj[i].question);
-
+function hideShow() {
+    if(myObj.length <= page){
+        document.getElementById(btnNxt).style.display = 'none';
+    } else {
+        document.getElementById(btnNxt).style.display = 'block';
     }
 }
+
+function buildQuiz(pg) {
+    var myQuestion = myObj[page].question;
+    var myCorrect = myObj[page].correct;
+    var questionHolder = '';
+    for(var i in myObj[page].answer){
+        questionHolder+='<li class="answer"><a href="#" class="answer-link">'+myObj[page].answer[i]+'</a></li>';
+    }
+    output.innerHTML = '<h2>'+myQuestion+' ?</h2>';
+    output.innerHTML += '<ul class="has-answer">'+questionHolder+'</ul>';
+    console.log(myObj[page].question);
+
+
+}
+// for (var i in myObj){
+//     console.log(myObj[i].question);
